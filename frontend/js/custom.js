cd76e09931d610d652402a84fda3c1c96abc6a46
Mihai Tiuca <teodor.tiuca@gmail.com>
2018-04-11 20:23:20 -0400

    update site with videos

---
+++ frontend/js/custom.js
@@ -0,0 +1,23 @@
+var mobileAndTabletcheck = function () {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$(document).ready(function () {
+  if(mobileAndTabletcheck()) {
+    $('video').attr("controls", "true");
+  } else {
+    $('video').hover(function toggleControls() {
+      if (this.hasAttribute("controls")) {
+        this.removeAttribute("controls");
+      } else {
+        this.setAttribute("controls", "controls");
+      }
+    });
+  }
+
+  $('video').click(function(){this.paused?this.play():this.pause();});
+});
